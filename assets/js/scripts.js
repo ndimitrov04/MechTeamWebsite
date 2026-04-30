4069da4cb2c95978dae74b8cb6f7a3a9a626339b
NikX <NikolaDimitrov04@protonmail.com>
2026-04-30 14:50:22 +0300

    Added game trailer (click on home banner); Website now fully mobile compatible; Updated game explanation images

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -48,3 +48,41 @@ function updateThemeImages(isDark) {
 }
 const isDark = document.body.classList.contains("dark-mode");
 updateThemeImages(isDark);
+
+
+
+const container = document.getElementById("trailer-container");
+
+function setVideoSize() {
+    const width = container.offsetWidth;
+    const height = width * 9 / 16; // 16:9 ratio
+    container.style.height = height + "px";
+}
+
+// Swap image for trailer
+container.addEventListener("click", function () {
+
+    // Set video container size based on screen format
+    setVideoSize();
+
+    // Apply styles to container in order to accomodate for videoplayer
+    this.style.paddingBottom = "5px";
+    this.style.boxSizing = "border-box";
+
+    // Add videoplayer
+    this.innerHTML = `
+        <iframe 
+            width="100%" 
+            height="90%" 
+            style="padding-top: 5px; box-sizing: border-box;"
+            src="https://www.youtube.com/embed/9HDxTwkZfA0?autoplay=1&controls=0&modestbranding=1&rel=0&fs=0&vq=hd2160" 
+            frameborder="0" 
+            allow="autoplay; encrypted-media" 
+            allowfullscreen>
+        </iframe>
+    `;
+});
+
+// Update videocontainer proportions on resize
+window.addEventListener("resize", setVideoSize);
+window.addEventListener("orientationchange", setVideoSize);
